a3c64919154f3b8ed9b80a14c8cb78e19cba47e0
geb <dmsql@DESKTOP-VV8AR2J>
2020-10-07 17:36:15 +0900

    z

--- src/main/java/project/club/p003/controller/ClubP003_d001Controller.java
+++ src/main/java/project/club/p003/controller/ClubP003_d001Controller.java
@@ -1,6 +1,7 @@
 package project.club.p003.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,7 +9,7 @@ import project.club.vo.ClubArticleVO;
 
 public interface ClubP003_d001Controller {
 	public ModelAndView writeArticleForm(String c_id) throws Exception;
-	public String writeArticle(ClubArticleVO vo, HttpServletRequest request) throws Exception;
 	public ModelAndView editArticle(String ca_id) throws Exception;
 	public void deleteClubArticle(String ca_id, String c_id) throws Exception;
+	public String writeArticle(ClubArticleVO vo, HttpServletRequest request, HttpSession session) throws Exception;
 }
