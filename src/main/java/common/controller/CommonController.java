472118d0fe6f9e5f41ecd3866e38240b87b6cba0
stothey0804 <stothey0804@nate.com>
2020-11-02 09:40:52 +0900

    소모임 알림

--- src/main/java/common/controller/CommonController.java
+++ src/main/java/common/controller/CommonController.java
@@ -3,6 +3,7 @@ package common.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import common.service.CommonService;
@@ -13,7 +14,19 @@ public class CommonController {
 	@Autowired
 	CommonService commonService;
 	
-
+	// 소모임명 조회
+	@RequestMapping(value = "/selectClubName.do", produces = "application/text; charset=utf8")
+	@ResponseBody
+	public String selectClubName(@RequestParam String c_id) {
+		return commonService.selectClubName(c_id);
+	}
+	
+	// 소모임장 아이디 조회
+	@RequestMapping("/selectClubManager.do")
+	@ResponseBody
+	public String selectClubManager(@RequestParam String c_id) {
+		return commonService.selectClubManager(c_id);
+	}
 	
 	@RequestMapping("/selectCommonCodeName.do")
 	@ResponseBody
--- src/main/java/common/service/CommonService.java
+++ src/main/java/common/service/CommonService.java
@@ -9,4 +9,6 @@ public interface CommonService {
 	public List<HashMap<String, String>> searchCommonCodeList(String g_id);
 	public String selectCommonCodeName(Map<String, String> param);
 	public String selectCommonGroupName(String g_id);
+	public String selectClubName(String c_id);	// 소모임명 조회
+	public String selectClubManager(String param); // 소모임장 아이디 조회
 }
