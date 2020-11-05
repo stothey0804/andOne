15c0603688533e2216ed959cf0c6926eb7d194db
SOJUNG <SOJUNG@DESKTOP-NQ2LI1J>
2020-11-06 05:10:33 +0900

    수정

--- src/main/java/project/root/p001/contoller/RootP001_d002ControllerImpl.java
+++ src/main/java/project/root/p001/contoller/RootP001_d002ControllerImpl.java
@@ -1,12 +1,14 @@
 package project.root.p001.contoller;
 
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,7 +16,7 @@ import common.Common;
 
 @Controller
 public class RootP001_d002ControllerImpl implements RootP001_d001Controller {
-	
+
 
 	// 메인영역
 //	@RequestMapping(value="/")
