875e3f7d80fb07430ccda82769ca7c329b8cb762
SOJUNG <SOJUNG@DESKTOP-NQ2LI1J>
2020-11-09 10:50:25 +0900

    콤마, 해쉬태그

---
+++ src/main/java/project/and/p001/controller/AndP001_d002ControllerImpl.java
@@ -0,0 +1,26 @@
+package project.and.p001.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import project.and.p001.service.AndP001_d002Service;
+
+@Controller
+public class AndP001_d002ControllerImpl implements AndP001_d002Controller {
+//	@Autowired
+//	private AndP001_d002Service p001_d001Service;
+//	
+//	@Override
+//	@RequestMapping(value="/admin/searchAndOneList.do")
+//	public ModelAndView searchAndOneList(@RequestParam(defaultValue = "1") int curPage,) {
+//		
+//		
+//		ModelAndView mav = new ModelAndView("AndOneAdmin");
+		
+//	}
+		
+
+}
